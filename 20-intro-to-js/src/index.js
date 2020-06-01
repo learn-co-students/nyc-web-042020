2a996e6c276604f722c2e0fc9c63914644f60550
Steven Doran <steven.doran@flatironschool.com>
2020-06-01 12:06:46 -0400

    post lecture

---
+++ 20-intro-to-js/src/index.js
@@ -0,0 +1,65 @@
+console.log("Welcome to Mod 3")
+
+/*
+ Questions
+
+ What is JS?
+
+ ECMAScript
+ - specification
+ - var, let or const
+ - .map
+ - browser enging (Chrome, Safari, FireFox), Nodejs
+ - "In ES6, you can do ....", ES2015
+
+ Why JS?
+
+  - three pillars
+  1. capturing user behavior/interaction
+  2. controlling what the user sees on the page
+  3. making HTTP requests
+ 
+  - Single Page Applications
+
+
+ How JS?
+
+  - inline script tage
+  - load local js files
+  - load js files from the internet - CDN
+
+
+ Some data types
+ -null
+  - essentially the same as `nil` in ruby
+  - indicates the absense of a value
+ 
+ - Number
+  - the one and only data type for numbers
+  - NaN is still a number
+  - 0 is falsey
+ 
+ - String
+  - double quotes or single quotes
+  - ruby: "My name is #{name}"
+  - js: `My name is ${name}`
+  - string1 + string2 => string1string2
+
+ - Object Literals
+  - equivalent to hashes in ruby
+  - sometimes called dictionaries in other languages
+  - a data type that holds key value pairs
+ 
+ - Array
+  - again, pretty much the same as ruby
+  - can hold multiple data types, even function
+  - iterators (forEach, map, reduce, filter)
+  - multi-dimensional arrays
+
+
+ - undefined
+ Functions
+
+
+*/
+
