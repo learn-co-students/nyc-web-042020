30d3d6a0150212a58a57f91d275d5adfcc8981fc
Steven Doran <steven.doran@flatironschool.com>
2020-06-03 10:27:58 -0400

    pre lecture

---
+++ 22-intro-to-events/listeners.js
@@ -0,0 +1,11 @@
+console.log("listening")
+
+// when a user clicks on the welcome image I want to see a turquoise border
+
+
+
+// when the mouse moves over the picture i want to see a picture of a baby sloth, and i want the picture to change back when the mouse moves off the image
+
+
+
+
