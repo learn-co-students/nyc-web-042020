dba758bdaa744f566b39e5df00761d230643129a
Steven Doran <steven.doran@flatironschool.com>
2020-06-03 11:37:11 -0400

    mid lecture

--- 22-intro-to-events/index.js
+++ 22-intro-to-events/index.js
@@ -1,1 +1,55 @@
 console.log("Events")
+
+// -----------
+
+//const 
+// variables declared with const cannot be redefined or redeclared
+
+// const steven = "Steven"
+// steven = "Stephen"
+
+// let michael = "Michael"
+// michael = "Mike"
+
+// -----------
+
+
+// add goonies to the DOM
+
+// √* create an li element for goonies
+// √* set the innerHTML to movie details
+// √* get the UL from the DOM
+// √* append the LI the UL
+
+const movieList = document.querySelector('#movie-list')
+
+const goonies = {
+  title: "The Goonies",
+  imageUrl: "https://images-na.ssl-images-amazon.com/images/I/519WqfZT2xL._AC_.jpg",
+  score: 0,
+  genre: "Adventure",
+  year: 1985
+}
+
+function createMovieLi(movieObj){
+  const movieLi = document.createElement('li')
+  movieLi.className = 'movie'
+  
+  movieLi.innerHTML = `
+    <h3>${movieObj.title}</h3>
+    <img alt="" src="${movieObj.imageUrl}" />
+    <h4>Year: ${movieObj.year}</h4>
+    <h4>Genre: ${movieObj.genre}</h4>
+    <h4>Score: <span>${movieObj.score}</span> </h4>
+    <button class="up-vote">Up Vote</button>
+    <button>Down Vote</button>
+    <button>&times;</button>
+  `
+
+  return movieLi
+}
+
+const gooniesLi = createMovieLi(goonies)
+movieList.append(gooniesLi)
+
+
--- 22-intro-to-events/listeners.js
+++ 22-intro-to-events/listeners.js
@@ -2,9 +2,20 @@ console.log("listening")
 
 // when a user clicks on the welcome image I want to see a turquoise border
 
+// √* get the image from the DOM
+// √* add a listener to image node to listen for clicks
+// √* when that node is clicked, we need to add a turquoise border to the image
 
+const welcomeImage = document.querySelector('#welcome-image')
 
-// when the mouse moves over the picture i want to see a picture of a baby sloth, and i want the picture to change back when the mouse moves off the image
+welcomeImage.addEventListener("click", function(e){
+  // e.target will always be the node the event just happend to
+  console.log("image click", e.target)
+  const image = e.target
+  image.style.border = "solid thick turquoise"
+})
+
+// when the mouse moves over the welcome image i want to see a picture of a baby sloth, and i want the picture to change back when the mouse moves off the image
 
 
 
