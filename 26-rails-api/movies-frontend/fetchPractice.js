f40a1ce43ec3cd2202237b5e63be67a5fb07ee10
Steven Doran <steven.doran@flatironschool.com>
2020-06-08 10:26:31 -0400

    pre lecture

---
+++ 26-rails-api/movies-frontend/fetchPractice.js
@@ -0,0 +1,23 @@
+/*
+
+To answer the following question please spin up the json-server database in this directory.
+ - `json-server -w db.json`
+
+You can test your code in the browser console. 
+Try to answer these questions without looking at any other code.
+
+
+1. Following RESTful conventions, what's the HTTP method and path used to create a new resource in the database (e.g., a movie)?
+
+2. What do POST, PATCH, and DELETE fetch requests require that a GET fetch request does not?
+
+3. Why do we not need to pass the id as a property of the body when we make a DELETE fetch request?
+
+4. Write the fetch request to get all the movies from the db and log the response to the console.
+
+5. Write the fetch request to change the title of "Jaws" to "Big Fish, Big Teeth".
+
+6. What data does json-server return when we make a POST request to create a new movie? What does it return when we make a DELETE request to remove a movie? How can we test this?
+
+*/
+
