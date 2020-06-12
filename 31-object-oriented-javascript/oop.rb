d56fac6a905ed857b85cbc92e7a605aa5f09489d
Steven Doran <steven.doran@flatironschool.com>
2020-06-12 13:38:44 -0400

    mid lecture

--- 31-object-oriented-javascript/oop.rb
+++ 31-object-oriented-javascript/oop.rb
@@ -2,12 +2,41 @@ require 'pry'
 
 class Person
   # age, name, interests, bio()
+  attr_accessor :age, :name, :interests
+
+  def initialize(age, name, interests)
+    @age = age
+    @name = name
+    @interests = interests
+  end
+
+  def bio
+    "#{@name} is #{age} years old and enjoys #{interests.join(', ')}"
+  end
 end
 
 class Student < Person
   # grade, greeting()
+  attr_accessor :grade
+  
+  def initialize(age, name, interests, grade)
+    super(age, name, interests)
+    @grade = grade
+  end
+
+  def greeting
+    "#{name} is in grade #{grade}. Cool. "
+  
+    
+    def bio
+      "something different"
+    end
+  
 end
 
+michael = Person.new('none of your biz', "Michael", ["Simpsons", "Harry Potter"])
+
+steven = Student.new('older than you think', "Steven", ["knitting", "spoon carving", "skiing"], 25)
 
 binding.pry
 puts 'okthxbye' 
